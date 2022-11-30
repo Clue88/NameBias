92ab8b9054a1853e54dee81a161f2b815680ce82
Christopher Liu <christopher.gy.liu@gmail.com>
2022-11-29 20:47:05 -0500

    Add convert to pickle

--- combine_names.py
+++ combine_names.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+import pandas as pd
+
 # The location of the pile_counts directory
 PILE_COUNTS = "pile_counts"
 
@@ -36,14 +38,36 @@ def combine_name_counts(files: list, output: str) -> None:
                         total_name_counts[key] = name_counts[key]
         except FileNotFoundError:
             print(f"Couldn't find file {file}")
-            
+
     with open(output, "w", encoding="utf-8") as f:
         f.write(json.dumps(total_name_counts))
 
     print("Done!")
 
 
+def convert_to_pickle(file: str, dest: str) -> None:
+    """
+    Save the given combined name counts file to Pickle file.
+    """
+    try:
+        with open(file, "r", encoding="utf-8") as f:
+            name_counts = json.loads(f.read())
+            name_items = name_counts.items()
+            name_df = pd.DataFrame.from_dict(
+                {
+                    "name": [name for name, _ in name_items],
+                    "count": [count for _, count in name_items],
+                }
+            )
+            name_df.to_pickle(dest)
+
+    except FileNotFoundError:
+        print(f"Couldn't find file {file}")
+
+
 if __name__ == "__main__":
     files = get_all_files(0, 30, PILE_COUNTS, ".txt")
     output = os.path.join(PILE_COUNTS, "combined.txt")
     combine_name_counts(files, output)
+    output_pickle = os.path.join(PILE_COUNTS, "combined.pickle")
+    convert_to_pickle(output, output_pickle)
