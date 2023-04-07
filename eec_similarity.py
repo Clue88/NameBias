8812175ebf91372c2384646c4384a70c5c131593
Christopher Liu <christopher.gy.liu@gmail.com>
2023-04-07 10:53:16 -0400

    Convert env vars to sge

--- eec_similarity.py
+++ eec_similarity.py
@@ -4,15 +4,14 @@ import pandas as pd
 
 from eec import compare_single_name
 
-ARRAY_N_TASKS = int(os.environ["SLURM_ARRAY_TASK_COUNT"])
-ARRAY_TASK_NUM = int(os.environ["SLURM_ARRAY_TASK_ID"])
+ARRAY_TASK_NUM = int(os.environ["SGE_TASK_ID"])
 
 def add_eec_similarity(pickle_path: str, dest: str):
     df = pd.read_pickle(pickle_path)
 
-    df = df.head(101)
+    df = df.head(100)
 
-    df = np.array_split(df, ARRAY_N_TASKS)[ARRAY_TASK_NUM]
+    df = np.array_split(df, 100)[ARRAY_TASK_NUM]
     df["ref_sim"] = df.apply(
         lambda x: compare_single_name(x["name"], x["sex"], "anger", "I feel angry"),
         axis=1,
