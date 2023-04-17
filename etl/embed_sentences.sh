b23510d1a8c2e45cf10d2d8f7438418832fa4220
Christopher Liu <christopher.gy.liu@gmail.com>
2023-04-17 14:23:08 -0400

    Embed sentences

---
+++ etl/embed_sentences.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# name of job
+#$ -N embed_sentences_sadness
+
+# working directory
+#$ -wd /nlp/data/liuchris/NameBias/
+
+#$ -pe parallel-onenode 4
+
+# join standard error and standard output of script into job_name.ojob_id
+#$ -j y -o embed_sentences_sadness.out
+
+env/bin/python3 embed_sentences.py
